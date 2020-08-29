36bdf6302733b4a853de9e006c58ab73bb635343
Trevor <trceniviva@gmail.com>
2020-08-29 16:17:13 -0400

    start of game done; show new avail done;

--- golf/golf.js
+++ golf/golf.js
@@ -13,20 +13,29 @@ let cards = [
     "queen-spades","queen-clubs","queen-hearts","queen-diamonds",
     "king-spades","king-clubs","king-hearts","king-diamonds"
 ]
+
+let playerTurn = true;
+
 let players = 2;
-var pl1 = document.getElementById("pl-1");
-var pl2 = document.getElementById("pl-2");
-var pl3 = document.getElementById("pl-3");
-var pl4 = document.getElementById("pl-4");
-var pl5 = document.getElementById("pl-5");
-var pl6 = document.getElementById("pl-6");
-var pc1 = document.getElementById("pc-1");
-var pc2 = document.getElementById("pc-2");
-var pc3 = document.getElementById("pc-3");
-var pc4 = document.getElementById("pc-4");
-var pc5 = document.getElementById("pc-5");
-var pc6 = document.getElementById("pc-6");
+let playerCardsFlipped = 0;
+let oppCardsFlipped = 0;
+let oppFlipped = [];
+
+var pl1 = document.getElementById("pl1");
+var pl2 = document.getElementById("pl2");
+var pl3 = document.getElementById("pl3");
+var pl4 = document.getElementById("pl4");
+var pl5 = document.getElementById("pl5");
+var pl6 = document.getElementById("pl6");
+var pc1 = document.getElementById("pc1");
+var pc2 = document.getElementById("pc2");
+var pc3 = document.getElementById("pc3");
+var pc4 = document.getElementById("pc4");
+var pc5 = document.getElementById("pc5");
+var pc6 = document.getElementById("pc6");
+
 const deck = document.getElementById("deck");
+const available = document.getElementById("available");
 const startGame = document.getElementById("start-game");
 const restart = document.getElementById("restart");
 
@@ -49,19 +58,63 @@ function shuffle(arra1) {
     return arra1;
 }
 
+let i = 0;
+
 function placeCards() {
     shuffle(cards);
-    pl1.src = "PNG/" + cards[0] + ".png";
-    pl2.src = "PNG/" + cards[1] + ".png";
-    pl3.src = "PNG/" + cards[2] + ".png";
-    pl4.src = "PNG/" + cards[3] + ".png";
-    pl5.src = "PNG/" + cards[4] + ".png";
-    pl6.src = "PNG/" + cards[5] + ".png";
+    available.src = "";
+    playerCardsFlipped = 0;
+    oppCardsFlipped = 0;
+    oppFlipped = [];
+    i = 0;
+    pl1.src = "PNG/red_back.png";
+    pl2.src = "PNG/red_back.png";
+    pl3.src = "PNG/red_back.png";
+    pl4.src = "PNG/red_back.png";
+    pl5.src = "PNG/red_back.png";
+    pl6.src = "PNG/red_back.png";
+    pc1.src = "PNG/red_back.png";
+    pc2.src = "PNG/red_back.png";
+    pc3.src = "PNG/red_back.png";
+    pc4.src = "PNG/red_back.png";
+    pc5.src = "PNG/red_back.png";
+    pc6.src = "PNG/red_back.png";
+    i = i + 12;
+    
+}
 
-    pc1.src = "PNG/" + cards[6] + ".png";
-    pc2.src = "PNG/" + cards[7] + ".png";
-    pc3.src = "PNG/" + cards[8] + ".png";
-    pc4.src = "PNG/" + cards[9] + ".png";
-    pc5.src = "PNG/" + cards[10] + ".png";
-    pc6.src = "PNG/" + cards[11] + ".png";
+function handleCardClick(cardClicked, cardNumber) {
+    if (playerCardsFlipped < 2) {
+        document.getElementById(cardClicked).src = "PNG/" + cards[cardNumber] + ".png"
+        playerTurn = false;
+        playerCardsFlipped = playerCardsFlipped + 1;
+    } else if (playerCardsFlipped < 5) {
+        document.getElementById(cardClicked).src = "PNG/" + cards[cardNumber] + ".png"
+        available.src = "PNG/" + cards[i] + ".png";
+        playerCardsFlipped = playerCardsFlipped + 1;
+        playerTurn = false;
+        flipOppCards();
+    } 
+}
+
+function flipOppCards() {
+    while (oppCardsFlipped < 3) {
+        let ranFlipVal = Math.ceil(Math.random() * 6);
+        if (oppFlipped.includes(ranFlipVal)) {;}
+        else {
+            oppFlipped.push(ranFlipVal);
+            ranFlipID = "pc" + ranFlipVal;
+            document.getElementById(ranFlipID).src = "PNG/" + cards[ranFlipVal+5] + ".png";
+            oppCardsFlipped = oppCardsFlipped + 1;}
+    }
+    playerTurn = true;
+}
+
+function flipCard(cardClicked, cardNumber) {
+    document.getElementById(cardClicked).src = "PNG/" + cards[cardNumber] + ".png"
+}
+
+function flipNewAvailable() {
+    i = i +1;
+    available.src = "PNG/" + cards[i] + ".png";
 }
